5ccb2a417b0481303930dc12a5ea9b7f563a6d83
ilunga lebreton <ilungalebreton@gmail.com>
2020-03-28 21:36:22 -0400

    commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://lebretoni:Idl2014.@roulette-express-f4eo8.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'roulette'
+};
